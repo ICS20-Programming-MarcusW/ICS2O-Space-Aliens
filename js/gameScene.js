43bec4c6e4d53a88e5085af79197c6e692b0c5d9
MarcusW <99274214+MarcusWehbi@users.noreply.github.com>
2022-06-08 03:34:45 +0000

    Created missiles

--- js/gameScene.js
+++ js/gameScene.js
@@ -13,6 +13,7 @@ class GameScene extends Phaser.Scene {
     super({ key: 'gameScene' })
     this.background = null
     this.ship = null
+    this.fireMissile = false
   }
 
   /* Get the scene up and running, initialize data object with content of another data object (our particular scene). 
@@ -29,6 +30,7 @@ class GameScene extends Phaser.Scene {
     // images
     this.load.image('gameSceneBackground', 'images/raceTrack.jpg')
     this.load.image('ship', 'images/skyCar.webp')
+    this.load.image('missile', 'images/tire.png')
   }
 
   /* Creating a new object by using an existing object as the prototype for the new object. Used to create game objects. 
@@ -38,6 +40,8 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, 'gameSceneBackground').setScale(5.0)
     this.background.setOrigin(0, 0)
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship').setScale(0.12)
+    // create a group for the missiles
+    this.missileGroup = this.physics.add.group()
   }
 
   /* Replacing old content of the element with new provided content, and returning the element. This method is called once 
@@ -47,6 +51,7 @@ class GameScene extends Phaser.Scene {
     // called 60 times a second.
     const keyLeftObj = this.input.keyboard.addKey('LEFT')
     const keyRightObj = this.input.keyboard.addKey('RIGHT')
+    const keySpaceObj = this.input.keyboard.addKey('SPACE')
     
     if (keyLeftObj.isDown === true) {
       this.ship.x = this.ship.x - 15
@@ -56,10 +61,24 @@ class GameScene extends Phaser.Scene {
       this.ship.x = this.ship.x + 15
     }
 
+    // wrap sprite to other side once it exceeds limit
     if (this.ship.x < 0) {
       this.ship.x = 1920
     } else if (this.ship.x > 1920) {
       this.ship.x = 0
+    }
+
+    if (keySpaceObj.isDown === true) {
+      if (this.fireMissile === false) {
+        // fire missile
+        this.fireMissile = true
+        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
+        this.missileGroup.add(aNewMissile)
+      }
+    }
+
+    if (keySpaceObj.isUp === true) {
+      this.fireMissile = false
     }
   }  
 }
